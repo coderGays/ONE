ecfcd66d57cf787016307f5192e867b937e8e210
TimorYang <iyangzhidong@gmail.com>
2016-06-24 19:51:35 +0800

    xxxx

---
+++ one/one/Classes/ONE1111111.h
@@ -0,0 +1,13 @@
+//
+//  ONE1111111.h
+//  one
+//
+//  Created by TimorYang on 16/6/24.
+//  Copyright © 2016年 TimorYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ONE1111111 : UIView
+
+@end
---
+++ one/one/Classes/ONE1111111.m
@@ -0,0 +1,21 @@
+//
+//  ONE1111111.m
+//  one
+//
+//  Created by TimorYang on 16/6/24.
+//  Copyright © 2016年 TimorYang. All rights reserved.
+//
+
+#import "ONE1111111.h"
+
+@implementation ONE1111111
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
